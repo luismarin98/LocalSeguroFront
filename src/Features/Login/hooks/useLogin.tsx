693995bc829de8e32dfcfa040c5d01844c6a89a7
AndyMDV <jun19981708@hotmail.com>
2024-02-23 12:11:53 -0500

    updatingApis

--- src/Features/Login/hooks/useLogin.tsx
+++ src/Features/Login/hooks/useLogin.tsx
@@ -5,10 +5,11 @@ import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
     const navigate = useNavigate();
+    const api = process.env.REACT_APP_API_USERS ? process.env.REACT_APP_API_USERS : 'http://localhost:3001/api/users'
 
     const getUser = async (data: UserRequest) => {
         try {
-            const response = await axios.get<UserRequest[]>(`http://localhost:3001/api/users/?username=${data.username}&password=${data.password}`);
+            const response = await axios.get<UserRequest[]>(`${api}?username=${data.username}&password=${data.password}`);
             if (response.data.length > 0 && response.data[0].username === data.username) {
                 toast.success(`Bienvenid@ ${response.data[0].username}`);
                 navigate('/dashboard');
