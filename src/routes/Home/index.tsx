cae5ff9fcfa297d470ba96bac31b4122402ccd78
luismarin98 <jun19981708@hotmail.com>
2024-03-16 19:13:16 -0500

    updates

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -13,12 +13,12 @@ export const Home = () => {
     }, [])
     
     return (
-        <div className="flex flex-col gap-3 items-center justify-center h-screen">
+        <div className="flex flex-col gap-3 items-center justify-center h-screen w-screen p-2">
             <p className="text-6xl text-center">Hola!</p>
             <p className="text-xl text-center">Bienvenid@ a la pagina "Local Seguro"</p>
-            <p className='text-md'>Actualmente esta no es una pagina oficial de la policia nacional</p>
-            <p className="text-md">ya que es una pagina dedicada para el estudio</p>
-            <p className="text-sm">Att. El desarrollador</p>
+            <p className='text-md text-center'>Esta no es una pagina oficial de la policia nacional</p>
+            <p className="text-md text-center">ya que es una pagina dedicada para el estudio</p>
+            <p className="text-sm text-center">Att. El desarrollador</p>
         </div>
     )
 }
